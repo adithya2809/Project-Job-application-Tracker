d7578808dd7d4ff64e4452f97a191845e82cfe3f
adithya2809 <agneyadityavardhan@gmail.com>
2026-08-18 20:52:21 +0530

    feat: implement login functionality with form handling and server communication

--- app/main.py
+++ app/main.py
@@ -2,12 +2,19 @@ from fastapi import FastAPI
 from app.routers import applications,auth
 from app import models
 from app.database import Base,engine
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI()
 
 app.include_router(applications.router)
 app.include_router(auth.router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
--- react-frontend/src/login.jsx
+++ react-frontend/src/login.jsx
@@ -1,13 +1,20 @@
 import { useState } from "react";
-
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
 function Login(){
     const [formData,setFormData]=useState({
         "user_name":"",
         "email":"",
         "password":""
-    }
+    });
+    const [message,setMessage]=useState("");
+    const [loginError,setLoginError]=useState("");
+    const [loginLoading,setLoginLoading]=useState(false);
 
-    );
+
+    
     function handleChange(event){
         const {name,value}=event.target;
 
@@ -16,19 +23,53 @@ function Login(){
             [name]:value
         })
     }
-export default Login;
-}
+
+    async function handleSubmit(event){
+        event.preventDefault();
+        setMessage("");
+        setLoginError("");
+        setLoginLoading(true);
+
+        try{
+            const response=await fetch("http://localhost:8000/auth/login",{
+                method:"POST",
+                headers:{
+                    "content-Type":"application/json"
+                },
+                body:JSON.stringify(formData)
+            }
+        );
+            const data=await response.json();
+            if (response.ok){
+                localStorage.setItem("token",data.access_token);
+                setMessage("Login Successful")
+            }
+            else{
+                setLoginError(data.detail)
+            }
+    }
+        catch(error){
+            setLoginError("Unable to connect to the server")
+        }
+
+        finally{
+            setLoginLoading(false)
+        }
+    }
 
 return (
     <>
     <h1>Login Page</h1>
-    <input type="text" name="username" value={formData.user_name}
+    <form onSubmit={handleSubmit}>
+    <input type="text" name="user_name" value={formData.user_name}
         onChange={handleChange}/>
     <input type="email" name="email" value={formData.email}
         onChange={handleChange}/>
     <input type="password" name="password" value={formData.password}
         onChange={handleChange}/>
-    <button type="submit">Login</button>
-
+    <button type="submit" disabled={loginLoading}>{loginLoading?"logging in...":"Login"}</button>
+    </form>
     </>
 )
+}
+export default Login;
