0007c6505c12ca715c5578aa3a984d257e9f5059
adithya2809 <agneyadityavardhan@gmail.com>
2026-08-19 21:45:08 +0530

    feat: add editing functionality for job applications in dashboard*

--- react-frontend/src/dashboard.jsx
+++ react-frontend/src/dashboard.jsx
@@ -9,6 +9,19 @@ const [applications,setApplications]=useState([]);
 const [loading,setLoading]=useState(true);
 const [error,setError]=useState("");
 
+const [editingId,setEditingId]=useState(null);
+
+const [editFormData,seteditFormData]=useState({
+    company: "",
+    role: "",
+    location: "",
+    job_type: "",
+    status: "",
+    application_date: "",
+    job_url: "",
+    salary: "",
+    notes: ""
+});
 
 useEffect(()=>{
     async function getApplications(){
@@ -40,7 +53,7 @@ useEffect(()=>{
     async function handleDelete(id){
         try{
             const token=localStorage.getItem("token");
-            const response=await fetch("http://localhost:8000/applications/${id}",{
+            const response=await fetch(`http://localhost:8000/applications/${id}`,{
                 method:"DELETE",
                 headers:{Authorization: `Bearer ${token}` }
             }
@@ -57,9 +70,18 @@ useEffect(()=>{
 
         
     }
-    delete_application();
-
-
+    
+    function handleChange(event){
+        const {name,value}=event.target;
+        seteditFormData({
+            ...editFormData,
+            [name]:value
+        }
+        );
+    }
+    function handleEdit(application){
+        const 
+    }
 return(
     <>
     <h1>Dashboard</h1>
@@ -79,8 +101,14 @@ return(
             <p>{application.job_url}</p>
             <p>{application.salary}</p>
             <p>{application.notes}</p>
-            <button>Edit</button>
-            <button onClick={()=>{handleDelete.id}}>Delete</button>
+            {editingId==application.id ? <input type="text" 
+            name="company"
+            value={editFormData.company} 
+            onChange={handleChange}></input>
+            :application.company}
+            
+            <button onClick={()=>{setEditingId(application.id)}}>Edit</button>
+            <button onClick={()=>{handleDelete(application.id)}}>Delete</button>
         </div>
     )}
 
